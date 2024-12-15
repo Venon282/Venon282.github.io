2c1b0444d1a7fd9dec26e05b14bda86f00f40ab6
Venon282 <esto500@hotmail.com>
2024-12-15 11:59:18 +0100

    test navigation

--- router.js
+++ router.js
@@ -1,28 +1,28 @@
-import {getContent} from '/js/file.js'
+// import {getContent} from '/js/file.js'
 
-const routes = {
-    '': 'html/home.html',
-    'about': 'html/about.html',
-    'contact': 'html/contact.html',
-};
+// const routes = {
+//     '': 'html/home.html',
+//     'about': 'html/about.html',
+//     'contact': 'html/contact.html',
+// };
 
-function loadPage(path, to_element='content', css=true) {
-    let content_path=routes[path] || routes['/']
-    console.log('content_path',content_path)
-    load(to_element, content_path)
+// function loadPage(path, to_element='content', css=true) {
+//     let content_path=routes[path] || routes['/']
+//     console.log('content_path',content_path)
+//     load(to_element, content_path)
 
-    if(css){
-        let css_path = 'css/' + (path ? path : 'home') +'.css'
-        console.log('css_path',css_path)
-        let link = document.getElementById('cssCustom')
-        getContent(css_path).then(([content, status]) =>{
-            if(status==200)
-                link.setAttribute('href', content)
-            else
-                throw new Error(`Error loading ${css_path}: ${content}`)
-        })
-    }
-}
+//     if(css){
+//         let css_path = 'css/' + (path ? path : 'home') +'.css'
+//         console.log('css_path',css_path)
+//         let link = document.getElementById('cssCustom')
+//         getContent(css_path).then(([content, status]) =>{
+//             if(status==200)
+//                 link.setAttribute('href', content)
+//             else
+//                 throw new Error(`Error loading ${css_path}: ${content}`)
+//         })
+//     }
+// }
 
 function load(id, path='', default_enxtension='html'){
     const element_id = document.getElementById(id)
@@ -30,7 +30,7 @@ function load(id, path='', default_enxtension='html'){
     if(!path)
         path = default_enxtension+'/'+id+'.'+default_enxtension
 
-    console.log('path ', path)
+    console.log('load path ', path)
 
     getContent(path).then(([content, status]) =>{
         if(status==200)
@@ -40,11 +40,11 @@ function load(id, path='', default_enxtension='html'){
     })
 }
 
-function navigateTo(path) {
-    window.history.pushState({}, path, window.location.origin + '#' + path);
-    console.log("nav ", path)
-    loadPage(path);
-}
+// function navigateTo(path) {
+//     window.history.pushState({}, path, window.location.origin + '#' + path);
+//     console.log("nav ", path)
+//     loadPage(path);
+// }
 
 window.addEventListener('DOMContentLoaded', () => {
     load('header')
