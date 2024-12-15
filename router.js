258e7c5dde29da3c365896b146cfea2c4ef0dfd7
Venon282 <esto500@hotmail.com>
2024-12-15 12:06:28 +0100

    test navigation

--- router.js
+++ router.js
@@ -1,28 +1,28 @@
 import {getContent} from '/js/file.js'
 
-// const routes = {
-//     '': 'html/home.html',
-//     'about': 'html/about.html',
-//     'contact': 'html/contact.html',
-// };
+const routes = {
+    '/': 'html/home.html',
+    '/about': 'html/about.html',
+    '/contact': 'html/contact.html',
+};
 
-// function loadPage(path, to_element='content', css=true) {
-//     let content_path=routes[path] || routes['/']
-//     console.log('content_path',content_path)
-//     load(to_element, content_path)
+function loadPage(path, to_element='content', css=true) {
+    let content_path=routes[path] || routes['/']
+    console.log('content_path',content_path)
+    load(to_element, content_path)
 
-//     if(css){
-//         let css_path = 'css/' + (path ? path : 'home') +'.css'
-//         console.log('css_path',css_path)
-//         let link = document.getElementById('cssCustom')
-//         getContent(css_path).then(([content, status]) =>{
-//             if(status==200)
-//                 link.setAttribute('href', content)
-//             else
-//                 throw new Error(`Error loading ${css_path}: ${content}`)
-//         })
-//     }
-// }
+    if(css){
+        let css_path = 'css' + (path.split('/')[1] ? path : '/home') +'.css'
+        console.log('css_path',css_path)
+        let link = document.getElementById('cssCustom')
+        getContent(css_path).then(([content, status]) =>{
+            if(status==200)
+                link.setAttribute('href', content)
+            else
+                throw new Error(`Error loading ${css_path}: ${content}`)
+        })
+    }
+}
 
 function load(id, path='', default_enxtension='html'){
     const element_id = document.getElementById(id)
@@ -43,7 +43,7 @@ function load(id, path='', default_enxtension='html'){
 function navigateTo(path) {
     // window.history.pushState({}, path, window.location.origin + '#' + path);
     console.log("nav to", path, 'origin ',window.location.origin)
-    // loadPage(path);
+    loadPage(path);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
