1984d883d4705683dd946cc16525680b2b614f52
Venon282 <esto500@hotmail.com>
2024-12-15 10:35:05 +0100

    test navigation

---
+++ js/file.js
@@ -0,0 +1,10 @@
+function getContent(path){
+    return fetch(path)
+        .then(response => {
+            if (!response.ok) {
+                return [response.status, 503]
+            }
+            return [response.text(), 200]
+        })
+        .catch(error => {return [error, 503]})
+}
--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-// router.js
+import getContent from 'js/file.js'
 
 const routes = {
     '/': 'html/home.html',
@@ -6,34 +6,34 @@ const routes = {
     '/contact': 'html/contact.html',
 };
 
-function loadPage(path, to_element='content') {
-    load(id=to_element, content=routes[path] || routes['/'])
+function loadPage(path, to_element='content', css=true) {
+    load(id=to_element, path=routes[path] || routes['/'])
+
+    if(css){
+        path = 'css' + (path || 'home') +'.css'
+        link = document.getElementById('cssCustom')
+        const [content, status] = getContent(path)
+        if(status==200)
+            link.setAttribute('href', content)
+        else
+            throw new Error(`Error loading ${id}: ${content}`)
+    }
 }
 
-function load(id, content='', default_enxtension='.html'){
+function load(id, path='', default_enxtension='html'){
     const element_id = document.getElementById(id)
 
-    if(!content)
-        content = id+default_enxtension
+    if(!path)
+        path = default_enxtension+'/'+id+'.'+default_enxtension
 
-    fetch(content)
-        .then(response => response.text())
-        .tehn(html => {
-            element_id.innerHTML = html
-        })
-        .catch(error => console.error('Error loading: ', id))
+    const [content, status] = getContent(path)
+
+    if(status==200)
+        element_id.innerHTML = content
+    else
+        throw new Error(`Error loading ${id}: ${content}`)
+
 }
-
-// function setupRouter() {
-//     const navbar = document.getElementById('navbar');
-//     navbar.innerHTML = `
-//         <a href="#/" onclick="navigateTo('/')">Home</a>
-//         <a href="#/about" onclick="navigateTo('/about')">About</a>
-//         <a href="#/contact" onclick="navigateTo('/contact')">Contact</a>
-//     `;
-
-//     window.onpopstate = () => loadPage(window.location.pathname);
-// }
 
 function navigateTo(path) {
     window.history.pushState({}, path, window.location.origin + path);
